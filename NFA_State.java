cf03981cb6392b5797d9c8f12047057d38f5cba1
Yomnaelsoufy <48134369+Yomnaelsoufy@users.noreply.github.com>
2021-05-07 19:10:31 +0200

    Update NFA_State.java

--- NFA_State.java
+++ NFA_State.java
@@ -2,33 +2,31 @@
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class NFA_State {
-
+public class NFA_State{
 
     private HashMap<Character, HashSet<NFA_State>  > transitions ;
 
-    private HashSet< NFA_State > empty_transitions ;
-
-    private NFA_State( HashSet< NFA_State > empty_transitions,HashMap<Character, HashSet<NFA_State>  > transitions){
-        this.empty_transitions=empty_transitions;
-        this.transitions=transitions;
+    public void setEmpty_transitions(HashSet<NFA_State> empty_transitions) {
+        this.empty_transitions = empty_transitions;
     }
 
+    public HashSet<NFA_State> getEmpty_transitions() {
+        return empty_transitions;
+    }
+
+    private HashSet< NFA_State > empty_transitions ;
 
     public NFA_State() {
         transitions = new HashMap<>();
         empty_transitions = new HashSet<>();
     }
+
     public void setTransitions(HashMap<Character, HashSet<NFA_State>> transitions) {
         this.transitions = transitions;
     }
 
     public HashMap<Character, HashSet<NFA_State>> getTransitions() {
         return transitions;
-    }
-
-    public HashSet<NFA_State> transition ( char input ){
-        return transitions.get(input);
     }
 
     public void insert_transition ( char x , NFA_State s ){
