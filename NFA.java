9715b8d2407fbea7b4d80bc614b9ec7122b6d402
Yomnaelsoufy <48134369+Yomnaelsoufy@users.noreply.github.com>
2021-05-07 10:09:47 +0200

    Update NFA.java

--- NFA.java
+++ NFA.java
@@ -1,34 +1,25 @@
-import java.io.Serializable;
-import java.util.HashMap;
+
 import java.util.HashSet;
 
-public class NFA implements Serializable {
+public class NFA  {
+
     private NFA_State start_state;
 
     private HashSet<NFA_State> accepting_states;
 
     private HashSet<NFA_State> states;
 
-    public NFA(NFA_State start_state,HashSet<NFA_State> accepting_states,HashSet<NFA_State> states){
-        this.start_state=start_state;
-        this.accepting_states=accepting_states;
-        this.states=states;
-    }
-
-
-    public HashSet<NFA_State> getStates() {
-        return states;
-    }
-
-    public void setStates(HashSet<NFA_State> states) {
-        this.states = states;
-    }
-
     public NFA(NFA_State start_state) {
         this.start_state = start_state;
         accepting_states = new HashSet<>();
         states = new HashSet<>();
     }
+    
+    public HashSet<NFA_State> getStates() {
+        return states;
+    }
+
+
 
     public NFA_State getStart_state() {
         return start_state;
